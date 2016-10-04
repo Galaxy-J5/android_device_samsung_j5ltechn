4d1ea651bae51cd0def1498aea21435df0b11a00
Nick Verse <nikoskalomiris@gmail.com>
2016-10-04 20:50:48 +0300

    Update init_j5ltechn.c

--- init/init_j5ltechn.c
+++ init/init_j5ltechn.c
@@ -58,6 +58,7 @@ void init_msm_properties(unsigned long msm_id, unsigned long msm_ver, char *boar
       property_set("ro.build.description", "j5ltezm-user 5.1 LMY47O J5008ZMU1APE1 release-keys");
       property_set("ro.product.model", "SM-J5008");
       property_set("ro.product.device", "j5ltechn");
+
    }
 
    property_get("ro.product.device", device);
